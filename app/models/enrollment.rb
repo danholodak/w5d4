1fc1a5accc6069e784c7afd99b458f6550373007
Dan Holodak <danholodak@gmail.com>
2023-02-09 14:39:21 -0500

    Associate Enrollment

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -9,5 +9,17 @@
 #  updated_at :datetime         not null
 #
 class Enrollment < ApplicationRecord
+
+    
+    has_many :students,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+
+    belongs_to :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+
     
 end
